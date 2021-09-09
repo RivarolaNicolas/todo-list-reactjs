ff6d9f8f9812933a1208a96c12246623ce451a22
Nicolas Rivarola <RivarolaNicolas94@gmail.com>
2021-09-09 00:14:30 -0300

    Adds delete task functionality. Improves the UI.

--- src/components/ToDoElement.js
+++ src/components/ToDoElement.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const ToDoElement = (props) => {
-  return <div className="block m-1 bg-red-400 ">{props.task}</div>;
+  return (
+    <div className="flex flex-grow justify-center text-center ml-4 py-2 my-2 bg-red-400 ">
+      {props.task}
+    </div>
+  );
 };
 
 export default ToDoElement;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -16,21 +16,44 @@ const ToDoList = () => {
     event.preventDefault();
   }
 
+  const handleRemoveItem = (name) => {
+    const filteredArray = tasks.filter((item) => item !== name);
+
+    setTasks([...filteredArray]);
+  };
+
   return (
     <div>
       <Header />
       <form onSubmit={handleSubmit}>
-        <label>
+        <label className="justify-center align-middle text-center mx-2 px-2 py-2">
           Things to do:
           <br />
-          <input type="textarea" placeholder="Log your tasks!" onChange={handleChange} />
+          <textarea
+            rows="4"
+            cols="50"
+            className=" justify-center align-middle text-center flex-grow mx-2 px-2 py-2"
+            placeholder="Log your tasks!"
+            onChange={handleChange}
+          />
         </label>
-        <input type="submit" value="Submit" />
+        <br />
+        <input
+          type="submit"
+          className="justify-center align-middle  bg-yellow-500 hover:bg-yellow-700 text-white text-center font-bold my-2 mx-6 px-2 py-2 border border-yellow-700 rounded"
+          value="Submit"
+        />
       </form>
       {tasks.map
-        ? tasks.map((value) => (
-            <div className="container mx-auto w-auto">
-              <ToDoElement task={value} key={value} />
+        ? tasks.map((item, index) => (
+            <div className="flex">
+              <ToDoElement id={item} task={item} key={item} />
+              <button
+                className="flex  bg-purple-500 hover:bg-purple-700 text-white text-center font-bold my-2 mx-6 px-2 py-2 border border-purple-700 rounded"
+                onClick={() => handleRemoveItem(item)}
+              >
+                Remove
+              </button>
             </div>
           ))
         : 'tasks.map is not a function'}
